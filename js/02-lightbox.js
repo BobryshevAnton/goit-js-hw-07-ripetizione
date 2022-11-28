f91b8432914e06e2563c63818303dc1791722ab3
Anton Bobryshev <101340267+BobryshevAnton@users.noreply.github.com>
2022-11-28 14:07:15 +0100

    add second leson

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const ulList = document.querySelector( ".gallery" );
+
+const itemElement = galleryItems.map( ( { preview, original, description } ) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+} );
+
+ulList.insertAdjacentHTML( "afterbegin", itemElement.join( "" ) );
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData:"alt",captionDelay: 250 });
